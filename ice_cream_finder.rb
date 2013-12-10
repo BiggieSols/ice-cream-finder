593688e39a3e822ae60e1827f6cd3c97f1f74e6c
App Academy Student <student@appacademy.io>
2013-12-09 16:01:56 -0800

    using google geocoding API to get current location

--- ice_cream_finder.rb
+++ ice_cream_finder.rb
@@ -13,8 +13,23 @@ require 'launchy'
 #
 # https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&sensor=false&key=AddYourOwnKeyHere
 
+#http://maps.googleapis.com/maps/api/geocode/output?parameters
 output_type = "json"
-location = "37.7810487,-122.4115092"
+
+
+geocoding_url = Addressable::URI.new(
+  :scheme => "https",
+  :host => "maps.googleapis.com",
+  :path => "/maps/api/geocode/#{output_type}",
+  :query_values => { :address => "1061 Market St, San Francisco, CA", :sensor => false }
+).to_s
+
+#Launchy.open(geocoding_url)
+
+location = JSON.parse(RestClient.get(geocoding_url))
+coordinates = location["results"].first["geometry"]["location"].values
+
+location = coordinates.join(',')
 radius = 10000
 types = "food|restaurant"
 name = "ice cream"
@@ -23,12 +38,12 @@ key = "AIzaSyB0PuvFIBx2A1HVQ2PiQPQxC41yhuBfkGI"
 rankby = "distance"
 
 
-url = Addressable::URI.new(
+places_url = Addressable::URI.new(
   :scheme => "https",
   :host => "maps.googleapis.com",
   :path => "/maps/api/place/nearbysearch/#{output_type}",
   :query_values => { :location => location, :types => types, :sensor => false, :keywords => keywords, :key => key, :rankby => rankby }
 ).to_s
 
-p url
-json_results = Launchy.open(url)
+p places_url
+json_results = Launchy.open(places_url)
